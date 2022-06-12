b8d791d7ee5463fd483a9a0415b4c4868326995a
Marco Brandizi <marco.brandizi@gmail.com>
2022-06-12 18:28:28 +0100

    Upgrading jutils, rdfutils deps.

--- rdf2pg-core/src/main/java/uk/ac/rothamsted/kg/rdf2pg/test/DataTestUtils.java
+++ rdf2pg-core/src/main/java/uk/ac/rothamsted/kg/rdf2pg/test/DataTestUtils.java
@@ -9,7 +9,7 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.system.Txn;
 
 import info.marcobrandizi.rdfutils.namespaces.NamespaceUtils;
-import uk.ac.ebi.utils.io.IOUtils;
+import uk.ac.ebi.utils.opt.io.IOUtils;
 import uk.ac.rothamsted.kg.rdf2pg.pgmaker.support.rdf.RdfDataManager;
 
 /**
