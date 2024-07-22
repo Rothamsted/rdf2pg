c86f8f6f9a021083a4274942c6a339e8c1365a20
Marco Brandizi <marco.brandizi@gmail.com>
2024-07-22 17:32:25 +0100

    reactiveRead(), exception handling.

--- neo4j-utils/src/main/java/uk/ac/rothamsted/neo4j/utils/Neo4jReactorUtils.java
+++ neo4j-utils/src/main/java/uk/ac/rothamsted/neo4j/utils/Neo4jReactorUtils.java
@@ -4,13 +4,17 @@ import java.util.function.Function;
 
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Record;
+import org.neo4j.driver.exceptions.ClientException;
 import org.neo4j.driver.reactivestreams.ReactiveResult;
 import org.neo4j.driver.reactivestreams.ReactiveSession;
 import org.neo4j.driver.reactivestreams.ReactiveTransactionContext;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import uk.ac.ebi.utils.exceptions.ExceptionUtils;
 
 /**
  * Utilities to work with the Project Reactor integration into Neo4j.
@@ -23,6 +27,8 @@ import reactor.core.publisher.Mono;
  */
 public class Neo4jReactorUtils
 {
+	private static Logger log = LoggerFactory.getLogger ( Neo4jReactorUtils.class );
+	
 	/**
 	 * Helper to process a Neo4j read query in a reactive style.
 	 * 
@@ -59,19 +65,33 @@ public class Neo4jReactorUtils
 		return Flux.usingWhen (
 			// The reactive session is generated when a subscriber comes...
 			Mono.fromSupplier ( () -> neoDriver.session ( ReactiveSession.class ) ),
+			
 			// ...and it's used in the closure, to spawn a Flux of Rs
-			rsession -> 
-				rsession.executeRead ( tx ->
-				  // This yields a ReactiveResult
-					Mono.fromDirect ( callBack.apply ( tx ) )
-					// which is mapped onto its records
-					.flatMapMany ( ReactiveResult::records )
-					// and then records are mapped by our custom mapper, the result is Flux<R>
-					.map ( recordMapper )
-				) // executeRead()	
-			, // usingWhen(), closure publisher
-			ReactiveSession::close // usingWhen(), flux cleanup 
+			rsession -> rsession.executeRead ( tx ->
+			  // This yields a ReactiveResult
+				Mono.fromDirect ( callBack.apply ( tx ) )
+				// which is mapped onto its records
+				.flatMapMany ( ReactiveResult::records )
+				// and then records are mapped by our custom mapper, the result is Flux<R>
+				.map ( recordMapper )
+			), // executeRead(), usingWhen(), closure publisher
+			
+			ReactiveSession::close, // usingWhen(), flux cleanup in case of completion
+			
+			// usingWhen(), flux cleanup in case of error
+			(rsession, ex) ->
+				Flux.error ( ExceptionUtils.buildEx ( 
+					ClientException.class, ex, "Error while running reactive Neo4j query: $cause"
+				)),
+				
+			// usingWhen(), flux cleanup in case of cancelling
+			rsession -> {
+				log.debug ( "Neo4j reactive query cancelled" );
+				return rsession.close ();
+			}
+			
 		); // usingWhen ()
-	}
-
+	
+	} // static reactiveRead()
+	
 }
